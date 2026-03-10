e7d1a380359e3a23a28867ffdc935f7ee82ce5ee
Sharath Kumar Reddy <51874859+mr-sharath@users.noreply.github.com>
2026-03-10 08:53:55 +0530

    Update from Bolt.diy

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,27 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: ["./index.html", "./src/**/*.{js,jsx}"],
-  theme: { extend: {} },
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {
+      colors: {
+        navy: {
+          50: '#f2f5f9',
+          100: '#e1e9f2',
+          200: '#c8d7e7',
+          300: '#a1bbd6',
+          400: '#7398c0',
+          500: '#537aa8',
+          600: '#41618d',
+          700: '#364f73',
+          800: '#2f4360',
+          900: '#1e3a5f',
+          950: '#1a2a44',
+        },
+      },
+    },
+  },
   plugins: [],
-};
+}
